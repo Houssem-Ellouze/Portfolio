66d93cf88dfd04430fa75a29ba2cf42aef6b53d4
Houssem Ellouze <148000626+Houssem-Ellouze@users.noreply.github.com>
2023-11-12 21:22:48 +0100

    Java Script

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: [" Full-Stack Developer"," Data Scientist"," Data Analyst"," Business Analyst"],
+    typeSpeed : 100,
+    backspeed : 100,
+    backDelay: 1000,
+    loop : true
+})
